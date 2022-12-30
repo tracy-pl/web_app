4db776287082aed8901caa05f7dbebd8d1bad403
ypetrenko <vekaev4@gmail.com>
2022-12-30 09:57:13 +0100

    add env vers

--- src/constants/http.ts
+++ src/constants/http.ts
@@ -1,4 +1,1 @@
-export const API_BASE_URL =
-  process.env.NODE_ENV !== 'development'
-    ? 'https://tracy-server-devci.up.railway.app/api'
-    : 'http://localhost:3001/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
